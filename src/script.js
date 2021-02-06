c6ad3435c6f05458456b08052d8e0f16435b9514
Kristel Pau <77642513+kristelpau@users.noreply.github.com>
2021-02-07 00:25:34 +0200

    Added search function

--- src/script.js
+++ src/script.js
@@ -32,9 +32,21 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d0a1eea228acff23d10fc0e8adceec54"; 
-let city = "New York"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "d0a1eea228acff23d10fc0e8adceec54"; 
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit)
 
